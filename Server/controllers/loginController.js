24e3b2ed534ca070fe51aabf574a14ba3ebea793
Khouchane Mahmoud <97807779+Uno486@users.noreply.github.com>
2023-10-10 20:37:45 +0100

    Update loginController.js

--- Server/controllers/loginController.js
+++ Server/controllers/loginController.js
@@ -37,14 +37,12 @@ export const loginUser=async(req,res)=>
    
     res.cookie('token', generateToken({id:user.id,name:user.name,pic:user.pic,email:user.email}), {
       httpOnly: true,
-      sameSite: 'None',
       secure: process.env.NODE_ENV === 'production', // Set to true in production
       domain:process.env.NODE_ENV === 'production'?"talkative12.netlify.app":"localhost"
     });
     
     res.cookie('refreshToken', generateRefreshToken({id:user.id,name:user.name,pic:user.pic,email:user.email}), {
       httpOnly: true,
-      sameSite: 'None',
       secure: process.env.NODE_ENV === 'production',
       domain:process.env.NODE_ENV === 'production'?"talkative12.netlify.app":"localhost" // Set to true in production
    
